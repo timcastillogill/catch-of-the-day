58394263b69b1395d76e3680cfa6a41c2615ec74
timcastillogill <timgillviola@gmail.com>
2021-12-09 08:46:31 +0000

    refactor: break store picker into a component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-
-class StorePicker extends React.Component {
-  render() {
-    return <p>I am a Store Picker!</p>;
-  }
-}
+import StorePicker from "./components/StorePicker";
 
 render(<StorePicker />, document.querySelector("#main"));
