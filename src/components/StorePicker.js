b409bd2fcd512b2f43e2ec669500550834e5024b
timcastillogill <timgillviola@gmail.com>
2021-12-14 07:56:18 +0000

    feat: add on click event and function to render a new page for selected store

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,26 @@
 import React from "react";
-import { selector } from "stylus/lib/functions";
-import { formatException } from "stylus/lib/utils";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store →</button>
       </form>
     );
