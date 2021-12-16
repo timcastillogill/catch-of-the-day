28b6e143264d3be3f3d5cd20b3d9560b0b3716e7
timcastillogill <timgillviola@gmail.com>
2021-12-16 07:36:11 +0000

    build: Add Load Fish button and create functions to import object into App

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
   state = {
@@ -20,14 +22,22 @@ class App extends React.Component {
     });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
+        <ul className="fishes">{Object.keys(this.state.fishes)}</ul>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
